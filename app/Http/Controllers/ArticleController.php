ff504b293b311caf698db56916f5260e0b1de37e
delacroix <delacroix.jon@gmail.com>
2026-03-10 11:27:41 +0100

    Activité 10

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,7 @@ class ArticleController extends Controller
 {
     public function show($id)
     {
-        return view('pages.article-details', ['id' => $id]);
+        // return view('pages.article-details', ['id' => $id]);
+        return view('pages.article-details', compact('id'));
     }
 }
