ab17480fba9feee13c04705914be82aa2e52a104
delacroix <delacroix.jon@gmail.com>
2026-03-10 14:59:20 +0100

    Activité 13

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,10 @@
     <h2>Bienvenue sur le site de {{ $name }}</h2>
     <h3>Articles</h3>
     @foreach ($articles as $article)
-        <x-article :titre="$article['titre']" :description="$article['description']" />
+        @if ($loop->last)
+            @break
+        @else
+            <x-article :titre="$article['titre']" :description="$article['description']" />
+        @endif
     @endforeach
 @endsection
